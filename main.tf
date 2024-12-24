310df0aa0cc368aecbf7edb61d95fc6f43ec7ae2
Nikhil919-devops <nikhilrao1919@gmail.com>
2024-12-24 16:35:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "my_pets" {
  filename = "/home/ec2-user/pets.txt"
  content = "I love Pets !" 
 }
+resource "random_pet" "my-pet" {
+  prefix = "MR"
+  separator = "."
+  length = "1"
+}
